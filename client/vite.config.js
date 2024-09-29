f8940e5abc7dda46439e96a17b0c200d439c066b
Aaron <8454621+sinwailam193@users.noreply.github.com>
2024-09-29 19:32:53 +0800

    Update to add library

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,13 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
+const PORT = 3000;
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        TanStackRouterVite({ quoteStyle: "double", disableTypes: true }),
+        TanStackRouterVite({
+            quoteStyle: "double",
+            disableTypes: true,
+            semicolons: true,
+        }),
         react(),
     ],
     server: {
-        port: 3000,
+        port: PORT,
+    },
+    preview: {
+        port: PORT,
     },
 });
